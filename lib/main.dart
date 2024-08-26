71781cb3dc06f745bea8ea917a2485e4187aae07
santoAppDebug <santomartin111@gmail.com>
2024-08-26 13:04:02 +0530

    new updats

--- lib/main.dart
+++ lib/main.dart
@@ -12,51 +12,51 @@ import 'global/constants/providers/providers_list.dart';
 import 'global/constants/routes/routes.dart';
 import 'package:path_provider/path_provider.dart';
 
-void main()async {
-    WidgetsFlutterBinding.ensureInitialized(); 
- Directory document = await getApplicationDocumentsDirectory();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Directory document = await getApplicationDocumentsDirectory();
   Hive.init(document.path);
   Hive.registerAdapter(UserModelAdapter());
-   Hive.registerAdapter(CheckInCheckOutStatusAdapter());
+  Hive.registerAdapter(CheckInCheckOutStatusAdapter());
 
- userBox =  await Hive.openBox<UserModel>(Config.dbName);
+  userBox = await Hive.openBox<UserModel>(Config.dbName);
 
-  runApp(MultiProvider(providers: providersList,
-    child: const MyApp()));
+  runApp(MultiProvider(providers: providersList, child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
-  return  Platform.isIOS
-            ? ScreenUtilInit(designSize: const Size(333,675),
-              child: CupertinoApp(
-                  title: 'FitnessApp',
-                  routes: routes,
-                  initialRoute: '/',
-                  theme:  CupertinoThemeData(
-                      barBackgroundColor:KColors().kWhite, primaryColor: Colors.black),
-                
-                  debugShowCheckedModeBanner: false,
-                  localizationsDelegates: const [
-                      DefaultWidgetsLocalizations.delegate,
-                      DefaultMaterialLocalizations.delegate,
-                      DefaultCupertinoLocalizations.delegate
-                    ]),
-            )
-            : ScreenUtilInit(designSize: const Size(333,675),child: MaterialApp(
-                 routes:routes ,
+    return Platform.isIOS
+        ? ScreenUtilInit(
+            designSize: const Size(333, 675),
+            child: CupertinoApp(
+                title: 'FitnessApp',
+                routes: routes,
+                initialRoute: '/',
+                theme: CupertinoThemeData(
+                    barBackgroundColor: KColors().kWhite,
+                    primaryColor: Colors.black),
+                debugShowCheckedModeBanner: false,
+                localizationsDelegates: const [
+                  DefaultWidgetsLocalizations.delegate,
+                  DefaultMaterialLocalizations.delegate,
+                  DefaultCupertinoLocalizations.delegate
+                ]),
+          )
+        : ScreenUtilInit(
+            designSize: const Size(333, 675),
+            child: MaterialApp(
+                routes: routes,
                 title: 'FitnessApp',
                 initialRoute: '/',
                 debugShowCheckedModeBanner: false,
                 theme: ThemeData.light().copyWith(
                   searchBarTheme: SearchBarThemeData(
-                      backgroundColor: MaterialStateProperty.all(KColors().kWhite)),
-                ))
-                );
+                      backgroundColor:
+                          MaterialStateProperty.all(KColors().kWhite)),
+                )));
   }
 }
-
