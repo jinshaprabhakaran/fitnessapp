82b2a1f6ebac0181ecec9d087fb9c5f5181a663f
santoAppDebug <santomartin111@gmail.com>
2024-08-26 18:16:36 +0530

    changes

--- lib/modules/dashboard/controller/dashboard_controller.dart
+++ lib/modules/dashboard/controller/dashboard_controller.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 import 'package:appdebug_font_package/appdebug_font_package.dart';
 import 'package:fitnessappadmin/global/constants/db_key.dart';
@@ -13,9 +12,8 @@ import 'package:intl/intl.dart';
 import '../widget/registration_data.widget.dart';
 import '../widget/upload_photo.widget.dart';
 
-class DashBoardController extends ChangeNotifier{
- 
- // controllers
+class DashBoardController extends ChangeNotifier {
+  // controllers
   TextEditingController nameCtrl = TextEditingController();
   TextEditingController ageCtrl = TextEditingController();
   TextEditingController phoneCtrl = TextEditingController();
@@ -24,69 +22,72 @@ class DashBoardController extends ChangeNotifier{
   TextEditingController bloodCtrl = TextEditingController();
 
   // bottom navigation bar screens
-  List screens = [const AttendanceScreen(),const RegistrationScreen(),const UsersListScreen()];
+  List screens = [
+    const AttendanceScreen(),
+    const RegistrationScreen(),
+    const UsersListScreen()
+  ];
 
 // selecting the index(bottom navigation)
   int selectedIndex = 0;
-setIndex(int index){
-selectedIndex = index;
-notifyListeners();
-}
+  setIndex(int index) {
+    selectedIndex = index;
+    notifyListeners();
+  }
 
 // page controller for pageview
 
-final PageController pageController = PageController(initialPage: 0);
+  final PageController pageController = PageController(initialPage: 0);
   int currentpage = 0;
-  setPage(int value){
+  setPage(int value) {
     currentpage = value;
     notifyListeners();
   }
-  
+
   // list
- List <UserModel> userModel = [];
-  List <UserModel> checkInList = [];
- List <UserModel> checkOutList = [];
- 
- // adding users from hive to an list
+  List<UserModel> userModel = [];
+  List<UserModel> checkInList = [];
+  List<UserModel> checkOutList = [];
+
+  // adding users from hive to an list
 
   addUserData() {
-  checkInList.clear();
-  checkOutList.clear();
-  userModel.clear();
+    checkInList.clear();
+    checkOutList.clear();
+    userModel.clear();
 
-  var currentDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
+    var currentDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
 
-  for (int i = 0; i < userBox.length; i++) {
-    var user = userBox.getAt(i) as UserModel?;
-    if (user != null) {
-      userModel.add(user);
-      userBox.get('dailyStatus');
-      notifyListeners();
-   
-      var status = user.dailyStatus?[currentDate];
-    
-    //  to track users checkin and checkout
-      if (status?.isCheckedOut == false) {
-       
-        checkOutList.add(user);
-      } else if (status == null || !status.isCheckedOut) {
-        checkInList.add(user);
+    for (int i = 0; i < userBox.length; i++) {
+      var user = userBox.getAt(i) as UserModel?;
+      if (user != null) {
+        userModel.add(user);
+        userBox.get('dailyStatus');
+        notifyListeners();
+
+        var status = user.dailyStatus?[currentDate];
+
+        //  to track users checkin and checkout
+        if (status?.isCheckedOut == false) {
+          checkOutList.add(user);
+        } else if (status == null || !status.isCheckedOut) {
+          checkInList.add(user);
+        }
       }
     }
+    notifyListeners();
   }
-  notifyListeners();
-}
 
- // when the user checkin, it moves to checkout list
- checkInUser(int index) {
+  // when the user checkin, it moves to checkout list
+  checkInUser(int index) {
     var user = checkInList[index];
     var currentDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
     user.dailyStatus = {};
     user.dailyStatus?[currentDate] = CheckInCheckOutStatus();
     user.dailyStatus?[currentDate]?.isCheckedIn = true;
-   
-  user.save();
-   checkInList.removeAt(index);
+
+    user.save();
+    checkInList.removeAt(index);
     if (!checkOutList.contains(user)) {
       checkOutList.add(user);
     }
@@ -94,53 +95,52 @@ final PageController pageController = PageController(initialPage: 0);
   }
 
 // when user checkedout, it removed from that list
-checkOutUser(int index) {
+  checkOutUser(int index) {
     var user = checkOutList[index];
     var currentDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
 
-      user.dailyStatus ??= {};
+    user.dailyStatus ??= {};
     user.dailyStatus?[currentDate] ??= CheckInCheckOutStatus();
     user.dailyStatus?[currentDate]?.isCheckedOut = true;
 
-    user.save(); 
-    
+    user.save();
+
     checkOutList.remove(user);
     notifyListeners();
   }
 
- // init function
- onInit(){ 
-  addUserData();
-  ageCtrl.clear();
-  nameCtrl.clear();
-  phoneCtrl.clear();
-  weightCtrl.clear();
-  heightCtrl.clear();
-  galleryFile = null;
- }
- // pick image from gallery and camera
+  // init function
+  onInit() {
+    addUserData();
+    ageCtrl.clear();
+    nameCtrl.clear();
+    phoneCtrl.clear();
+    weightCtrl.clear();
+    heightCtrl.clear();
+    galleryFile = null;
+  }
+
+  // pick image from gallery and camera
   File? galleryFile;
-  
+
   final picker = ImagePicker();
   Future getImage(
     ImageSource img,
   ) async {
     final pickedFile = await picker.pickImage(source: img);
     XFile? xfilePick = pickedFile;
-   
-        if (xfilePick != null) {
-          galleryFile = File(pickedFile!.path);
-        
-        } else {
-         
-        }
-        notifyListeners();
+
+    if (xfilePick != null) {
+      galleryFile = File(pickedFile!.path);
+    } else {}
+    notifyListeners();
   }
 
   showPicker({
     required BuildContext context,
   }) {
-    showModalBottomSheet(backgroundColor: KColors().kWhite,
+    showModalBottomSheet(
+      backgroundColor: KColors().kWhite,
       context: context,
       builder: (BuildContext context) {
         return SafeArea(
@@ -170,44 +170,78 @@ checkOutUser(int index) {
   }
 
 // registration flow
-  changePage(int index,BuildContext  context){
-    switch(index){
-    case 0:
-    return RegistrationDetails(title: 'Name',hintText: 'Name',
-      textEditingController:nameCtrl,value: 1 / 7,length: '1 / 7',);
-   case 1: 
-    return  RegistrationDetails(title: 'PhoneNumber',value: 2 / 7,length: '2 / 7',
-      hintText: 'Phone',textEditingController:phoneCtrl,);
-   case 2:  
-    return RegistrationDetails(title: 'Age',hintText: 'Age',textEditingController:ageCtrl,
-      value: 3 / 7,length: '3 / 7');
-    case 3: 
-     return RegistrationDetails(title: 'Weight',hintText: 'Kg',textEditingController:weightCtrl,
-      value: 4 / 7,length: '4 / 7');
-    case 4: 
-     return RegistrationDetails(title: 'Height',hintText: 'Cm',textEditingController:heightCtrl
-      ,value: 5 / 7,length: '5 / 7');
-    case 5: 
-     return RegistrationDetails(title: 'Blood Group',hintText: 'blood group',textEditingController:bloodCtrl,
-      value: 6 / 7,length: '6 / 7');
+  changePage(int index, BuildContext context) {
+    switch (index) {
+      case 0:
+        return RegistrationDetails(
+          title: 'Name',
+          hintText: 'Name',
+          textEditingController: nameCtrl,
+          value: 1 / 7,
+          length: '1 / 7',
+        );
+      case 1:
+        return RegistrationDetails(
+          title: 'PhoneNumber',
+          value: 2 / 7,
+          length: '2 / 7',
+          keyboardType: TextInputType.phone,
+          hintText: 'Phone',
+          textEditingController: phoneCtrl,
+        );
+      case 2:
+        return RegistrationDetails(
+          title: 'Age',
+          hintText: 'Age',
+          textEditingController: ageCtrl,
+          value: 3 / 7,
+          length: '3 / 7',
+          keyboardType: TextInputType.number,
+        );
+      case 3:
+        return RegistrationDetails(
+            title: 'Weight',
+            hintText: 'Kg',
+            textEditingController: weightCtrl,
+            keyboardType: TextInputType.number,
+            value: 4 / 7,
+            length: '4 / 7');
+      case 4:
+        return RegistrationDetails(
+            title: 'Height',
+            hintText: 'Cm',
+            textEditingController: heightCtrl,
+            keyboardType: TextInputType.number,
+            value: 5 / 7,
+            length: '5 / 7');
+      case 5:
+        return RegistrationDetails(
+            title: 'Blood Group',
+            hintText: 'blood group',
+            textEditingController: bloodCtrl,
+            value: 6 / 7,
+            length: '6 / 7');
       case 6:
-      return UploadPhotoWidget(title: 'Upload Photo', length: '7 / 7', value: 7 / 7, onPressed: ()async{
-     await  userBox.add(UserModel(name: nameCtrl.text, 
-         age: heightCtrl.text,weight:weightCtrl.text ,
-          height: heightCtrl.text,bloodgroup:bloodCtrl.text ,
-         phonenumber: phoneCtrl.text,
-          dp: galleryFile!.path), );
-         addUserData();
-    Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
-           });
+        return UploadPhotoWidget(
+            title: 'Upload Photo',
+            length: '7 / 7',
+            value: 7 / 7,
+            onPressed: () async {
+              await userBox.add(
+                UserModel(
+                    name: nameCtrl.text,
+                    age: heightCtrl.text,
+                    weight: weightCtrl.text,
+                    height: heightCtrl.text,
+                    bloodgroup: bloodCtrl.text,
+                    phonenumber: phoneCtrl.text,
+                    dp: galleryFile!.path),
+              );
+              addUserData();
+              Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+            });
 
- default :  
-   
-
-
-
-
-    
+      default:
     }
   }
 }
--- lib/modules/dashboard/widget/registration_data.widget.dart
+++ lib/modules/dashboard/widget/registration_data.widget.dart
@@ -8,53 +8,74 @@ import '../controller/dashboard_controller.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class RegistrationDetails extends StatelessWidget {
-  const RegistrationDetails({super.key,required this.title,required this.hintText,required this.textEditingController,required this.value,required this.length});
-final String title;
-final String hintText;
-final TextEditingController textEditingController;
-final double value;
-final String length;
+  const RegistrationDetails(
+      {super.key,
+      required this.title,
+      required this.hintText,
+      required this.textEditingController,
+      required this.value,
+      required this.length,
+      this.keyboardType = TextInputType.emailAddress});
+  final String title;
+  final String hintText;
+  final TextEditingController textEditingController;
+  final double value;
+  final String length;
+  final TextInputType keyboardType;
 
   @override
   Widget build(BuildContext context) {
-   return Consumer<DashBoardController>(
-      builder: (context, dashboardCtrl, child) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-        children: [ 
-           Gap(20.h),
-        SmoothPageIndicator(
-                        controller: dashboardCtrl.pageController,
-                        count: 7,
-                        effect:  WormEffect(type: WormType.thin,
-                          dotHeight: 6,
-                          dotWidth: 20,
-                          dotColor: KColors().kBlack,
-                          activeDotColor: Colors.green,
-                        ),
-                      ),
-          Gap(40.h),
-         KStyles().semiBold20(text: 'Complete your registration'),  
-        Gap(20.h),
-       KStyles().med17(text: title),
-       Gap(10.h),
-       TextFieldWidget(
-        hintText: hintText, readOnly: false, boxWidth: double.infinity, textAlign: TextAlign.left, textColor: KColors().kBlack, textEditingController: textEditingController)
-   ,Gap(70.h),
-   SizedBox(width: double.infinity,height: 40.h,
-     child: ElevatedButton(style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-      onPressed: (){
-      dashboardCtrl.pageController.nextPage(
-                    duration: const Duration(milliseconds: 300),
-                    curve: Curves.ease,
-                  );
-                  
-     }, child: KStyles().med15(text: 'Next',color: KColors().kWhite)),
-   )
-       
-      ],),
-    );
-  });
+    return Consumer<DashBoardController>(
+        builder: (context, dashboardCtrl, child) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gap(20.h),
+            SmoothPageIndicator(
+              controller: dashboardCtrl.pageController,
+              count: 7,
+              effect: WormEffect(
+                type: WormType.thin,
+                dotHeight: 6,
+                dotWidth: 20,
+                dotColor: KColors().kBlack,
+                activeDotColor: Colors.green,
+              ),
+            ),
+            Gap(40.h),
+            KStyles().semiBold20(text: 'Complete your registration'),
+            Gap(20.h),
+            KStyles().med17(text: title),
+            Gap(10.h),
+            TextFieldWidget(
+                hintText: hintText,
+                readOnly: false,
+                boxWidth: double.infinity,
+                textAlign: TextAlign.left,
+                textColor: KColors().kBlack,
+                keyboardType: keyboardType,
+                textEditingController: textEditingController),
+            Gap(70.h),
+            SizedBox(
+              width: double.infinity,
+              height: 40.h,
+              child: ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                  onPressed: () {
+                    dashboardCtrl.pageController.nextPage(
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.ease,
+                    );
+                  },
+                  child:
+                      KStyles().med15(text: 'Next', color: KColors().kWhite)),
+            )
+          ],
+        ),
+      );
+    });
   }
 }
